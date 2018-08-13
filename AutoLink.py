bd6d6617f802a5cf6d0c32899dbf96053e0fb9a8
苦叶子 <lymking@foxmail.com>
2018-08-13 10:20:34 +0800

    新增AutoLink主入口源码

---
+++ AutoLink.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "苦叶子"
+
+"""
+
+公众号: 开源优测
+
+Email: lymking@foxmail.com
+
+"""
+import os
+
+from flask_script import Manager
+
+from auto.www.app import create_app, load_all_task
+from auto.settings import HEADER
+from utils.help import check_version
+
+os.environ["PATH"] = os.environ["PATH"] + ";" + os.getcwd() + "/driver"
+
+print(HEADER)
+
+app = create_app('default')
+manager = Manager(app)
+
+
+if __name__ == '__main__':
+
+    check_version()
+
+    load_all_task(app)
+
+    manager.run()
