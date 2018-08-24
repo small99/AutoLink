35b1f40f3202807a1fe16c7bfbf0841322c4f1c0
苦叶子 <lymking@foxmail.com>
2018-08-24 15:35:45 +0800

    新增文件类型映射icon图标定义

---
+++ utils/resource.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "苦叶子"
+
+"""
+
+公众号: 开源优测
+
+Email: lymking@foxmail.com
+
+"""
+
+ICONS = {
+    ".robot": "icon-robot",
+    ".txt": "icon-resource",
+    ".bmp": "icon-image",
+    ".jpg": "icon-image",
+    ".jpeg": "icon-image",
+    ".png": "icon-image",
+    ".gif": "icon-image",
+    ".py": "icon-python",
+    ".xls": "icon-excel",
+    ".xlsx": "icon-excel"
+}
