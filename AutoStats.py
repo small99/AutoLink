118b3b04b598057b7f3426287f6353e1cbd3d83c
苦叶子 <lymking@foxmail.com>
2018-08-27 14:26:16 +0800

    优化代码统计功能

--- AutoStats.py
+++ AutoStats.py
@@ -17,7 +17,7 @@ basedir = os.getcwd()
 filelists = []
 
 # 指定想要统计的文件类型
-whitelist = ['py']
+whitelist = ['py', 'js']
 
 filelists = []
 
@@ -27,11 +27,15 @@ def get_file(base_dir):
     for parent, dirnames, filenames in os.walk(basedir):
         # for dirname in dirnames:
         # getFile(os.path.join(parent,dirname)) #递归
+
         for filename in filenames:
-            if filename == "AutoStats.py":
+            if filename in ("AutoStats.py", "commonLibrary.py"):
                 continue
 
             ext = filename.split('.')[-1]
+            if ext == "js" and filename != "auto.js":
+                continue
+
             # 只统计指定的文件类型，略过一些log和cache文件
             if ext in whitelist:
                 filelists.append(os.path.join(parent, filename))
@@ -54,5 +58,6 @@ if __name__ == '__main__' :
     totalline = 0
     for filelist in filelists:
         totalline = totalline + count_line(filelist)
+
     print('total lines: %s' % totalline)
     print('Done! Cost Time: %0.2f second' % (time.clock() - startTime))
